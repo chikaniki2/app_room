e648b23be95847c9237bd12d0990724a58be5d86
chikaniki2 <mac@macnoMacBook-Pro.local>
2022-02-24 17:01:33 +0900

    user_roomが正常に作成できていなかったので修正

--- db/migrate/20220224070835_create_user_rooms.rb
+++ db/migrate/20220224070835_create_user_rooms.rb
@@ -1,8 +1,8 @@
 class CreateUserRooms < ActiveRecord::Migration[6.1]
   def change
     create_table :user_rooms do |t|
-      t.date_field :date_start
-      t.date_field :date_end
+      t.date :date_start
+      t.date :date_end
       t.integer :price
       t.integer :user_id
       t.integer :room_id
